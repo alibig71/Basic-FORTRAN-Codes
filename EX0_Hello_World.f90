06983ac6088a4ea066e1516041f0db6bd4cc722f
Alireza Bigdeli <55369594+alibig71@users.noreply.github.com>
2020-06-22 23:58:58 -0300

    Add files via upload

---
+++ EX0_Hello_World.f90
@@ -0,0 +1,18 @@
+
+
+!*************************************
+!    
+!  PROGRAM: Hello World 
+!  Example 00
+!  By Alibig71
+!  Feb 2020
+!     
+!**************************************
+
+    program Helworld
+
+    implicit none
+
+     print *, 'Hello World'
+     pause
+    end program Helworld
